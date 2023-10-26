8c88d6891664d9c34a334f687b7601857040f6d7
dat-nguyen-304 <nguyenphatdat3004@gmail.com>
2023-10-26 18:03:06 +0700

    fix CEPA-116 set fixed for sider and header

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
+
 import React, { useState } from 'react';
-import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Drawer, Layout, Menu, theme } from 'antd';
 import Image from 'next/image';
@@ -35,14 +35,22 @@ const Sidebar: React.FC<SidebarProps> = ({ user, items, children }) => {
     };
 
     return (
-        <Layout style={{ minHeight: '100vh' }}>
+        <Layout style={{ minHeight: '100vh' }} className="relative">
             <Sider
                 className="hidden sm:block"
                 collapsible
                 collapsed={collapsed}
                 onCollapse={value => setCollapsed(value)}
+                style={{
+                    overflow: 'auto',
+                    height: '100vh',
+                    position: 'fixed',
+                    left: 0,
+                    top: 0,
+                    bottom: 0
+                }}
             >
-                <div className="flex flex-col items-center gap-4 my-4">
+                <div className="flex flex-col items-center gap-4 mb-4 mt-20">
                     <Image src={user.avatar} width={60} height={60} alt="" className="cursor-pointer !rounded-full" />
                     {!collapsed && <p className="text-white">{user.fullName}</p>}
                 </div>
@@ -60,22 +68,17 @@ const Sidebar: React.FC<SidebarProps> = ({ user, items, children }) => {
                 <Menu theme="light" defaultSelectedKeys={['1']} mode="inline" items={items} />
             </Drawer>
             <Layout>
-                <Header className="!bg-white flex h-[60px] justify-between items-center !px-[20px]">
+                <Header className="!bg-[#001529] flex h-[60px] justify-between items-center !px-[20px] fixed top-0 left-0 right-0 z-[999]">
                     <div className="flex gap-4 items-center">
-                        <Button className="hidden sm:block bg-transparent" onClick={() => setCollapsed(!collapsed)}>
-                            {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                        </Button>
-                        <div className="flex items-center gap-4">
-                            <Image
-                                src="https://intaadvising.gatech.edu/wp-content/uploads/2020/11/cepa.png"
-                                width={45}
-                                height={30}
-                                alt=""
-                                className="cursor-pointer"
-                            />
-                            <div onClick={() => setOpenDrawer(true)} className="block sm:hidden">
-                                <TbMenu2 size={20} />
-                            </div>
+                        <Image
+                            src="https://intaadvising.gatech.edu/wp-content/uploads/2020/11/cepa.png"
+                            width={45}
+                            height={30}
+                            alt=""
+                            className="cursor-pointer"
+                        />
+                        <div onClick={() => setOpenDrawer(true)} className="block sm:hidden">
+                            <TbMenu2 size={20} className="text-white" />
                         </div>
                     </div>
                     <div className="hidden sm:flex items-center justify-end h-[60px] sm:gap-[48px]">
@@ -93,7 +96,7 @@ const Sidebar: React.FC<SidebarProps> = ({ user, items, children }) => {
                         </Button>
                     </div>
                 </Header>
-                <Content className="">
+                <Content className={collapsed ? 'sm:ml-[80px] transition-all' : 'sm:ml-[200px] transition-all '}>
                     <div className="p-1 sm:p-[24px] min-h-[360px]" style={{ background: colorBgContainer }}>
                         {children}
                     </div>
