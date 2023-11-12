e3374021a6c6e27295087c27d9f8b8dd39d72e45
dat-nguyen-304 <nguyenphatdat3004@gmail.com>
2023-11-12 22:03:24 +0700

    feat CEPA-198 integrate api courses(teacher)

--- api-client/course-api.ts
+++ api-client/course-api.ts
@@ -5,5 +5,10 @@ export const courseApi = {
     getAll: async (page: number) => {
         const res = await axiosClient.get(`/courses?page=${page}&size=20&sortType=ASC`);
         return res.data;
+    },
+
+    getAllOfTeacher: async (email: string, page: number) => {
+        const res = await axiosClient.get(`/courses/teacher?email=${email}&page=${page}&size=20&sortType=ASC`);
+        return res.data;
     }
 };
